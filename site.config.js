fb8c2e71d8aa712ef73e57886bb15c5be9dbf19b
BenU <ngluonghungg2611@gmail.com>
2024-01-24 13:12:05 +0700

    upadte link cfg

--- site.config.js
+++ site.config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log-ngluonghungg2611s-projects.vercel.app",
+  link: "https://bentech-blogs.vercel.app",
   since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
